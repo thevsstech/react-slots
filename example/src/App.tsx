151275b3c368c928e2088b8da958b1859aefe16b
vahitserifsaglam <vahit.serif119@gmail.com>
2021-06-16 15:00:50 +0300

    initial message

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,5 @@
-import React from 'react'
-
-import { ExampleComponent } from '@thevsstech/react-slots'
-import '@thevsstech/react-slots/dist/index.css'
-
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return null
 }
 
 export default App
